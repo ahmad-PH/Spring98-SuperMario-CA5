2cdf0f95867c45a4e5c898a9001a038edacb6867
ahmad pourihosseini <seyedahmadpourihosseini@gmail.com>
2019-03-22 14:30:42 +0430

    Fix a bug in intersection detection function

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -1,11 +1,28 @@
 #include "Geometry.h"
+#include "utility.h"
+
+
+#include <iostream>
+using namespace std;
 
 bool ExactRectangle::intersects(ExactRectangle r) {
-    return (x <= r.x + w &&
-            x + w >= r.x &&
-            y <= r.y + r.h &&
-            y + h >= r.y);
+
+//    if (compare_floats(r.x,64) && compare_floats(r.y,416)) {
+//        cout << "found rect: " << r << endl;
+//        cout<< "me: "<<*this<<endl;
+//        cout<<"decision: "<<res<<endl;
+//    }
+    return (x < r.x + w &&
+            x + w > r.x &&
+            y < r.y + r.h &&
+            y + h > r.y);
 }
 
 ExactRectangle::ExactRectangle(double _x, double _y, int _w, int _h) :
     x(_x), y(_y), w(_w), h(_h) {}
+
+std::ostream &operator<<(std::ostream &stream, const ExactRectangle r) {
+     return stream << "(x: " << r.x << ", y: " << r.y << ", w: " << r.w << ", h: " << r.h
+     << ")";
+}
+
