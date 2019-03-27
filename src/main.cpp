1235b0577cae8fa173852ede01d740d4b40267db
ahmad pourihosseini <seyedahmadpourihosseini@gmail.com>
2019-03-27 22:25:14 +0430

    Change main to read map address from command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "Game.h"
+#include<iostream>
 
-int main() {
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout<<"please provide a map name"<<endl;
+        exit(EXIT_FAILURE);
+    }
     Game game;
-    game.run_level("../assets/maps/1.txt");
+    game.run_level(argv[1]);
 }
